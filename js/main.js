9b996be701e9d64c956706f43f52cca5ca0ac312
Yazima <yazima04200@gmail.com>
2022-08-19 13:40:41 +0900

    完成

--- js/main.js
+++ js/main.js
@@ -4,14 +4,17 @@
   const question = document.getElementById('question');
   const choices = document.getElementById('choices');
   const btn = document.getElementById('btn');
+  const result = document.getElementById('result');
+  const scoreLabel = document.querySelector('#result > p');
 
-  const quizSet = [
-    {q: 'What is A?', c: ['A0', 'A1', 'A2']},
-    {q: 'What is B?', c: ['B0', 'B1', 'B2']},
-    {q: 'What is C?', c: ['C0', 'C1', 'C2']},
-  ];
+  const quizSet = shuffle([ // ここでshuffleを入れたことでクイズがランダムで表示される
+    {q: '世界で一番大きな湖は？', c: ['カスピ海', 'カリブ海', '琵琶湖']},
+    {q: '2の8乗は？', c: ['256', '64', '1024']},
+    {q: '次のうち、最初にリリースされた言語は？', c: ['Python', 'JavaScript', 'HTML']},
+  ]);
   let currentNum = 0;
   let isAnswered; // 一度選択肢を選んだら他のものは選択できない設定にするために必要
+  let score = 0; // 問題の正答数を管理するために必要
 
   // クイズ選択肢とシャッフル処理
   function shuffle(arr) {
@@ -32,6 +35,7 @@
 
     if (li.textContent === quizSet[currentNum].c[0]) {
       li.classList.add('correct');
+      score++; // 正答数を管理するために必要
     } else {
       li.classList.add('wrong');
     }
@@ -58,13 +62,28 @@
       });
       choices.appendChild(li);
     });
+
+    if (currentNum === quizSet.length - 1) { // 最後の問題ではnextのテキストをShow Scoreに変える処理
+      btn.textContent = 'Show Score';
+    }
   }
 
   setQuiz();
 
   // nextクリック後に次の問題が表示されるようにする処理
   btn.addEventListener('click', () => {
-    currentNum++;
-    setQuiz();
+    if (btn.classList.contains('disabled')) {
+      return;
+    } // disabledクラスが付いていたら次の問題にいかないようにreturnとする。
+    btn.classList.add('disabled'); // そのうえで次の問題に行くときはbtnをグレーに戻したいのでここでdisabledクラスを付ける。
+
+    if (currentNum === quizSet.length - 1) { // 最後の回答後にShow Scoreをクリックするとスコア確認が可能に //
+      // console.log(`Score: ${score} / ${quizSet.length}`);
+      scoreLabel.textContent = `Score: ${score} / ${quizSet.length}`;
+      result.classList.remove('hidden');
+    } else {
+      currentNum++;
+      setQuiz();
+    }
   });
 }
